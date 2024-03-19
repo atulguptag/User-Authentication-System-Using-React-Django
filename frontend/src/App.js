1368c455aca169f29e8216a2be3bd2af7c678c96
Atul Gupta <atulguptag111@gmail.com>
2024-03-19 18:37:49 +0530

    Changed the API endpoint.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ axios.defaults.xsrfHeaderName = "X-CSRFToken";
 axios.defaults.withCredentials = true;
 
 const client = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://atul-backend-api.easify.xyz",
   withCredentials: true,
   headers: {
     Accept: "application/json",
