851de81306b23365ce1c99ce0432e69b5c223ffc
Atul Gupta <atulguptag111@gmail.com>
2024-03-19 16:14:48 +0530

    Added the username in the user_ob variable

--- backend/user_api/serializers.py
+++ backend/user_api/serializers.py
@@ -9,8 +9,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 		model = UserModel
 		fields = '__all__'
 	def create(self, clean_data):
-		user_obj = UserModel.objects.create_user(email=clean_data['email'], password=clean_data['password'])
-		user_obj.username = clean_data['username']
+		user_obj = UserModel.objects.create_user(email=clean_data['email'], username=clean_data['username'], password=clean_data['password'])
 		user_obj.save()
 		return user_obj
 
@@ -19,7 +18,7 @@ class UserLoginSerializer(serializers.Serializer):
 	password = serializers.CharField()
 	##
 	def check_user(self, clean_data):
-		user = authenticate(username=clean_data['email'], password=clean_data['password'])
+		user = authenticate(email=clean_data['email'], password=clean_data['password'])
 		if not user:
 			raise ValidationError('User not found')
 		return user
